2170dc2964d1c915800566ff93745a802a8325b5
yassine <yba73mot@gmail.com>
2024-03-01 23:20:33 +0100

    admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,14 @@
+const {
+  registerAdmin,
+  loginAdmin,
+} = require("../controllers/auth.controllers");
 const { getUsers } = require("../controllers/users.controllers");
 const { authenticateToken } = require("../middlewares/auth.middlewarese");
 const { isAdmin } = require("../middlewares/isAdmin");
 
 const router = require("express").Router();
+router.post("/register", registerAdmin);
+router.post("/login", loginAdmin);
 router.get("/users", authenticateToken, isAdmin, getUsers);
 
 module.exports = router;
